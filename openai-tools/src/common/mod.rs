6d6710bafd1d5d5b94436f4772e19f3300aefcdb
akitenkrad <akitenkrad@gmail.com>
2025-07-19 16:38:34 +0900

    chaged visibilities

--- openai-tools/src/common/mod.rs
+++ openai-tools/src/common/mod.rs
@@ -7,19 +7,11 @@ pub mod structured_output;
 pub mod tool;
 pub mod usage;
 
-#[allow(unused_imports)]
-use errors::{OpenAIToolError, Result};
-#[allow(unused_imports)]
-use function::Function;
-#[allow(unused_imports)]
-use message::{Content, Message, ToolCall};
-#[allow(unused_imports)]
-use parameters::{ParameterProp, Parameters};
-#[allow(unused_imports)]
-use role::Role;
-#[allow(unused_imports)]
-use structured_output::Schema;
-#[allow(unused_imports)]
-use tool::Tool;
-#[allow(unused_imports)]
-use usage::{CompletionTokenDetails, PromptTokenDetails, Usage};
+pub use errors::{OpenAIToolError, Result};
+pub use function::Function;
+pub use message::{Content, Message, ToolCall};
+pub use parameters::{ParameterProp, Parameters};
+pub use role::Role;
+pub use structured_output::Schema;
+pub use tool::Tool;
+pub use usage::{CompletionTokenDetails, PromptTokenDetails, Usage};
