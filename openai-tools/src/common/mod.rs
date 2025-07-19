e6708b889e3c4497df4580b5d22405ae15a62b21
akitenkrad <akitenkrad@gmail.com>
2025-07-19 16:31:37 +0900

    added imports in common/mod.rs

--- openai-tools/src/common/mod.rs
+++ openai-tools/src/common/mod.rs
@@ -6,3 +6,20 @@ pub mod role;
 pub mod structured_output;
 pub mod tool;
 pub mod usage;
+
+#[allow(unused_imports)]
+use errors::{OpenAIToolError, Result};
+#[allow(unused_imports)]
+use function::Function;
+#[allow(unused_imports)]
+use message::{Content, Message, ToolCall};
+#[allow(unused_imports)]
+use parameters::{ParameterProp, Parameters};
+#[allow(unused_imports)]
+use role::Role;
+#[allow(unused_imports)]
+use structured_output::Schema;
+#[allow(unused_imports)]
+use tool::Tool;
+#[allow(unused_imports)]
+use usage::{CompletionTokenDetails, PromptTokenDetails, Usage};
